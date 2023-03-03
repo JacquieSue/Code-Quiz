6455389682941a6d16a8ea9ef1213c2764d29d1d
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2023-03-03 10:36:19 -0700

    updated README.md

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # Code-Quiz
-Web APIs Challenge: Code Quiz
+Web APIs Challenge
 
+## Description
 
 In this project, I will work to create a coding quiz challenge application using HTML, CSS, and JavaScript
 
-## Instructions
+# Table of Contents
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
 
 This challenge will test my skills in all we've learned in coding bootcamp up to this point.
+
+## Usage 
 
 ### Task 1: HTML Build
 1. Create the header with highscores link and timer
 
-2. Create body with different 
-sections for questions
+2. Create body with different sections for questions
 
 ### Task 2: Style with CSS
 1. Add color to the buttons
@@ -21,3 +27,17 @@ sections for questions
 1. Add start quiz/sumbit button links
 
 2. Add text area to sumbit form
+
+[View deployed website](https://jacquiesue.github.io/Code-Quiz/)
+
+![Screenshot](assets/Screen%20Shot%202022-10-11%20at%209.44.48%20PM.png)
+
+## Credits
+Jacquelyn Pulliam
+
+[GitHub](https://github.com/JacquieSue)
+
+[Contact Me](jspulliam87@yahoo.com)
+
+## License
+MIT
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,56 @@
 // GIVEN I am taking a code quiz
 // put var(s) here:
-var startButton = document.querySelector(".start-button");
+var startButton = document.querySelector("#start-button");
 var timeEl = document.querySelector(".time");
+var startBox = document.querySelector("#start-box");
+var questionBox = document.querySelector("#question-box");
+var endBox = document.querySelector("#end-box");
+var highscoresBox = document.querySelector("#highscore-box");
+var clearHSButton = document.querySelector(".clear-high-scores");
+var questionEl = document.querySelector("#quesionEl");
+var choiceBox = document.getElementById("choice-box")
+// Selects element by id
+var mainEl = document.getElementById("main")
+
+var secondsLeft = 59;
+var questions = [{
+    question: "Arrays in JavaScript can be used to store.",
+    choice1: "JavaScript (Q1)",
+    choice2: "booleans (Q1)",
+    choice3: "alerts (Q1)",
+    choice4: "numbers (Q1)",
+    correct: "JavaScript (Q1)",
+},{
+    question: "this is question 2",
+    choice1: "I am choice 1(Q2)",
+    choice2: "I am choice 2(Q2)",
+    choice3: "I am choice 3(Q2)",
+    choice4: "I am choice 4 (Q2)",
+    correct: "I am choice 3(Q2)",
+},{
+    question: "this is question 3",
+    choice1: "I am choice 1(Q3)",
+    choice2: "I am choice 2(Q3)",
+    choice3: "I am choice 3(Q3)",
+    choice4: "I am choice 4 (Q3)",
+    correct: "I am choice 3(Q3)",
+},{
+    question: "this is question 4",
+    choice1: "I am choice 1(Q3)",
+    choice2: "I am choice 2(Q3)",
+    choice3: "I am choice 3(Q3)",
+    choice4: "I am choice 4 (Q3)",
+    correct: "I am choice 3(Q3)",
+},{
+    question: "this is question 5",
+    choice1: "I am choice 1(Q3)",
+    choice2: "I am choice 2(Q3)",
+    choice3: "I am choice 3(Q3)",
+    choice4: "I am choice 4 (Q3)",
+    correct: "I am choice 3(Q3)",
+    },]
+
+var questionsIndex = 0;
 // document.querySelectorAll()
 
 var HScount = localStorage.getItem("HScount");
@@ -15,141 +64,107 @@ function init() {
 // WHEN I click the start button
 // The startQuiz function is called
 function startQuiz() {
-    isCorrect = false;
-    timerCount = 59;
+secondsLeft= 59;
+questionsIndex =0;
     // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
     // The winGame function is called when the win condition is met
-
-    function completedQuiz() {
-        wordBlank.textContent = "Quiz Completed!";
-        winCounter++
-        startButton.disabled = false;
-    }
-    setHighscores()
-    renderBlanks()
+startBox.style.display="none";
+questionBox.style.display="block";
+   
+    // setHighscores()
+    // renderBlanks()
     startTimer()
+    displayQuestion()
 }
 // add start button
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startQuiz);
 
-// Add eventListener
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function (event) {
-    // If the count is zero, exit function
-    if (timerCount === 0) {
-        return;
+function displayQuestion (){
+questionEl.textContent = questions[questionsIndex].question
+document.getElementById("choice1El").textContent = questions[questionsIndex].choice1
+document.getElementById("choice2El").textContent = questions[questionsIndex].choice2
+document.getElementById("choice3El").textContent = questions[questionsIndex].choice3
+document.getElementById("choice4El").textContent = questions[questionsIndex].choice4
+}
+function completedQuiz() {
+    wordBlank.textContent = "Quiz Completed!";
+    winCounter++
+    startButton.disabled = false;
+}
+//THEN a timer starts and I am presented with a question
+// Selects element by class
+//var timeEl = document.querySelector(".time");
+// The setTimer function starts and stops the timer and triggers endQuiz()
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if correct answer is met
+            if (isCorrect && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                completedQuiz();
+            }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            endQuiz();
+        }
+    }, 1000);
+}
+
+
+//WHEN I answer a question
+function correctAnswer() {
+    //if the answer is correct        
+    if (choice === buttons.join("")) {
+        // This value is used in the timer function to test if win condition is met
+        isCorrect = true;
     }
 }
-    //THEN a timer starts and I am presented with a question
-    // Selects element by class
-    //var timeEl = document.querySelector(".time");
-    // The setTimer function starts and stops the timer and triggers endQuiz()
-    function startTimer() {
-        // Sets timer
-        timer = setInterval(function () {
-            timerCount--;
-            timerElement.textContent = timerCount;
-            if (timerCount >= 0) {
-                // Tests if correct answer is met
-                if (isCorrect && timerCount > 0) {
-                    // Clears interval and stops timer
-                    clearInterval(timer);
-                    completedQuiz();
-                }
-            }
-            // Tests if time has run out
-            if (timerCount === 0) {
-                // Clears interval
-                clearInterval(timer);
-                endQuiz();
-            }
-        }, 1000);
-    }
-    // Selects element by id
-    var mainEl = document.getElementById("main")
-
-    var secondsLeft = 59;
-
-    function setTime() {
-        // Sets interval in variable
-        var timerInterval = setInterval(function () {
-            secondsLeft--;
-
-            if (secondsLeft === 0) {
-                // Stops execution of action at set interval
-                clearInterval(timerInterval);
-                // Calls function to create and append image
-                sendMessage();
-            }
-
-        }, 1000);
-    }
-    setTime();
-
-    //WHEN I answer a question
-    function correctAnswer() {
-        //if the answer is correct        
-        if (choice === buttons.join("")) {
-            // This value is used in the timer function to test if win condition is met
-            isCorrect = true;
+//THEN I am presented with another question
+//create a loop
+function checkAnswer(event) {
+    console.log(event)
+    if(event.target.matches("button")){
+        if(event.target.textContent === questions[questionsIndex].correct){
+console.log("correct")
+        } else{
+console.log("wrong")
         }
-    }
-    //THEN I am presented with another question
-    //create a loop
-    function checkAnswer(options) {
-        var correctAnswer = true;
-        for (var i = 0; i < correctAnswer; i++) {
-            if (option[i] === correct) {
-                correctAnswer = true;
-            } else {
-                correctAnswer !== true
-            }
-
+        questionsIndex++ 
+        if(questionsIndex == questions.length){
+            endQuiz() 
+        } else {
+            displayQuestion()
         }
+        
+        
     }
-    wordBlank.textContent = blanksLetters.join(" ");
 
-
-    //WHEN I answer a question incorrectly
-    //make "if" statement
-
-    //THEN time is subtracted from the clock
-    //setInterval for seconds left
-    function setTime() {
-        // Sets interval in variable
-        var timerInterval = setInterval(function () {
-            secondsLeft--;
-
-            if (secondsLeft === 0) {
-                // Stops execution of action at set interval
-                clearInterval(timerInterval);
-                // Calls function to create and append image
-                sendMessage();
-            }
-
-        }, 1000);
-    }
-    setTime();
     
-    //WHEN all questions are answered or the timer reaches 0
-    //clear interval(timer interval)
+}
 
-    //THEN the game is over
-    //return ends the finction
+
+//WHEN all questions are answered or the timer reaches 0
+//clear interval(timer interval)
+
+//THEN the game is over
+//return ends the finction
 // The endQuiz function is called when timer reaches 0
 function endQuiz() {
-        wordBlank.textContent = "QUIZ OVER";
-        loseCounter++
-        startButton.disabled = false;
-        setLosses()
-    }
-    //WHEN the game is over
+    wordBlank.textContent = "QUIZ OVER";
+    loseCounter++
+    startButton.disabled = false;
+    setLosses()
+}
+//WHEN the game is over
 
-    //THEN I can save my initials and score
-    //Add reset button
-    var clearHSButton = document.querySelector(".clear-high-scores");
+//THEN I can save my initials and score
+//Add reset button
 
 function clearHighScores() {
     // Clears the high scores
@@ -178,3 +193,6 @@ function getHighscores() {
     //Render win count to page
     highscores.textContent = highscoresCounter;
 }
+// Attach event listener to start button to call startGame function on click
+startButton.addEventListener("click", startQuiz);
+choiceBox.addEventListener("click", checkAnswer);
